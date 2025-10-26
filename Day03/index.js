b0d916ca51290b2f30d37118dee185f0f9e3330b
sablu1234 <mdsablu0000000@gmail.com>
2025-10-26 23:38:29 +0600

    update

---
+++ Day03/index.js
@@ -0,0 +1,31 @@
+console.log("Day 03");
+
+// Operators - Symble + - * /
+// Oparends -x+y
+// Expression x =2 / 3+4
+
+let x = 4+5;
+
+// Arithmetic Operators
+
+let f_name = "tapas"
+let l_name = "Adhikari"
+console.log(f_name + l_name); // output: tapas Adhikari
+
+let a = 10;
+let b = 5;
+console.log(a + b); // output: 15
+console.log(a - b); // output: 5
+console.log(a * b); // output: 50
+console.log(a / b); // output: 2    
+console.log(b-a); // output: -5
+
+console.log(a ** b);
+console.log(a % b);
+
+
+let count = 5;
+console.log(count++); // output: 5
+console.log(count);
+
+console.log(++count); // output: 7
