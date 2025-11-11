8c95df8fdefcfec1a12a57f28f1fb292d4f0ac03
sablu1234 <mdsablu0000000@gmail.com>
2025-11-11 23:08:49 +0600

    day 13

--- Day13/index.js
+++ Day13/index.js
@@ -1,4 +1,4 @@
-"use strict";// if we use "use strict" then in function this=> undefined, but inside arrow function strict not work
+// "use strict";// if we use "use strict" then in function this=> undefined, but inside arrow function strict not work
 
 console.log("Day 13: the this keyword");
 
@@ -82,9 +82,117 @@ console.log("this inside the arrow fucntion defined in global scope", getFood())
 const food ={
     name: 'mango',
     color: 'yellow',
-    getDesc: () => `${this.name} is ${this.color}`,
-    // getDesc: function (){
-    //     return `${this.name} is ${this.color}`;
-    // }
+    // getDesc: () => `${this.name} is ${this.color}`,
+   /* getDesc: function (){
+        return `${this.name} is ${this.color}`;
+    }*/
+      getDesc: function(){
+       return () => `${this.name} is ${this.color}`;
+      }
 }
-console.log(food.getDesc());
+const descFun = food.getDesc();
+console.log(descFun());
+
+
+// Explicit binding -> call, apply, bind
+
+// The call method
+
+function greeting(){
+    console.log(`hello, ${this.name} belogns to ${this.address}`);
+}
+
+const user = {
+    name: 'tapascript',
+    address: 'All of You!'
+};
+
+greeting.call(user);
+
+
+
+const likes = function(hobby1,hobby2){
+    console.log(this.name + ' likes ' + hobby1 + ',' + hobby2);
+}
+
+const person = {
+    name: "tapas"
+}
+
+likes.call(person, 'Teaching', 'Blogging');
+
+// Apply method
+
+const hobbiesToApply = ["Sleepting","Eating"];
+
+likes.apply(person, hobbiesToApply);
+
+// bind() method
+
+const newHobbies = function(hobby1,hobby2){
+    console.log(this.name + " likes " + hobby1 + "," + hobby2);
+}
+
+const officer = {
+    name: "bob",
+}
+
+const newFn = newHobbies.bind(officer,"Dancing", "Singing");
+
+newFn();
+
+
+const Cartoon = function(name, animal){
+    this.name = name;
+    this.animal = animal;
+    this.log = function(){
+        console.log(this.name + ' is a ' + this.animal);
+    }
+};
+
+const tomCartoon = new Cartoon("Tom", "Cat");
+tomCartoon.log();
+
+const jerryCartoon = new Cartoon("Jerry", "Mouse");
+jerryCartoon.log();
+
+
+
+
+// const users = {
+//     name: "Tapas",
+//     greet: function(){
+//         function inner(){
+//             console.log(`Hello , ${this.name}!`);
+//         }
+//         inner();
+//     }
+// }
+
+// users.greet();
+
+
+
+// const users = {
+//     name: "Tapas",
+//     greet: function(){
+//         const inner =()=> {
+//             console.log(`Hello , ${this.name}!`);
+//         }
+//         inner();
+//     }
+// }
+
+// users.greet();
+
+
+
+const obj = {
+    name: "John",
+    greet: function(){
+        console.log(`Hello , ${this.name}!`);
+    }
+}
+
+const greetFn = obj.greet;
+greetFn.call(obj);
