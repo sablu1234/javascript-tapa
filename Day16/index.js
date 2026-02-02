3094b8a05a101dade2862f40f7ffce9d8a7dbbc1
sablu1234 <mdsablu0000000@gmail.com>
2026-02-02 21:47:29 +0600

    up

---
+++ Day16/index.js
@@ -0,0 +1,21 @@
+
+const print = function() {
+    const name = document.getElementById("m_name").value;
+    const wish = document.getElementById("m_wish").value;
+
+    const message = 'Hello ' 
+        + name 
+        + ', your wish "' 
+        + + wish 
+        + '" may come true 😊';
+
+    logger(message);
+
+    document.getElementById("output").innerHTML =
+        '<span class="message">' + message + '</span>';
+}
+
+const logger = function(text){
+    console.log("******I am a logger function*****");
+    console.log(text);
+}
