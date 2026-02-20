f09d0d884827872e38a2585736771f6baf9413d0
Hasan <mdsablu0000000@gmail.com>
2026-02-20 13:58:29 +0600

    up

--- Day16/index.js
+++ Day16/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 3094b8a05a101dade2862f40f7ffce9d8a7dbbc1
 const print = function() {
     const name = document.getElementById("m_name").value;
     const wish = document.getElementById("m_wish").value;
@@ -9,11 +5,11 @@ const print = function() {
     const message = 'Hello ' 
         + name 
         + ', your wish "' 
-<<<<<<< HEAD
+
         + wish 
-=======
+
         + + wish 
->>>>>>> 3094b8a05a101dade2862f40f7ffce9d8a7dbbc1
+
         + '" may come true 😊';
 
     logger(message);
