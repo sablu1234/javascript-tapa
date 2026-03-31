a2cd2a5821c7d653a5512893c7cf0e4e11e79ede
Hasan <mdsablu0000000@gmail.com>
2026-03-31 23:17:46 +0600

    dom

--- Day20/index.js
+++ Day20/index.js
@@ -5,3 +5,13 @@ const firstChild = parent.firstElementChild;
 console.log(firstChild);
 
 // Templates and cloning
+
+const template = document.getElementById("card-template");;
+const clone = template.content.cloneNode(true);
+clone.querySelector(".title").textContent = "Dom Advance Topic";
+clone.querySelector(".desc").textContent = "Hope your are learning something new";
+
+document.body.appendChild(clone);
+
+
+// Document Fragements
